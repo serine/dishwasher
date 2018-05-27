2de491061e7941cdb79a92c688963a3ae8fb9091
serine <tskiril@gmail.com>
2018-05-27 21:56:19 +1000

    This code works, but still doesn't stop at the end of the cycle :(

--- dishwasher.ino
+++ dishwasher.ino
@@ -10,72 +10,97 @@ int motorPin = 10;
 
 // cicle settings
 unsigned long oneMin = 60000;
-unsigned long initTime = oneMin/6;
-unsigned long fillTime = oneMin*2;
-unsigned long washTime = oneMin*42;
-unsigned long drainTime = oneMin;
-unsigned long rinseTime = oneMin*7;
+//unsigned long initTime = oneMin/6;
+//unsigned long fillTime = oneMin*2;
+//unsigned long washTime = oneMin*42;
+//unsigned long drainTime = oneMin;
+//unsigned long rinseTime = oneMin*7;
 
 void setup(){
   Serial.begin(9600);
   pinMode(solenoidPin, OUTPUT);
   pinMode(pumpPin, OUTPUT);
   pinMode(motorPin, OUTPUT);
-}
-
-void rinse(int n) {
-  //Rinse 1
-  digitalWrite(solenoidPin, HIGH);
-  delay(fillTime);
-  digitalWrite(solenoidPin, LOW);
-
-  digitalWrite(motorPin, HIGH);
-  delay(rinseTime/n);
-  digitalWrite(motorPin, LOW);
-
-  // Drain water
-  digitalWrite(pumpPin, HIGH);
-  delay(drainTime);
-  digitalWrite(pumpPin, LOW);
-
-  // rest
-  delay(oneMin*2);
-}
-
-void loop() {
-
-  delay(oneMin/6);
 
   /* Init */
 
+  delay(oneMin/6);
   digitalWrite(pumpPin, HIGH);
-  delay(initTime);
+  delay(oneMin/6);
   digitalWrite(pumpPin, LOW);
 
   /* Main */
 
   //Fill up with water 
   digitalWrite(solenoidPin, HIGH);
-  delay(fillTime);
+  delay(oneMin);
   digitalWrite(solenoidPin, LOW);
 
   // Start wash
   digitalWrite(motorPin, HIGH);
-  delay(washTime);
+  delay(oneMin*20);
   digitalWrite(motorPin, LOW);
 
   // rest
-  delay(oneMin*2);
+  delay(oneMin);
 
   // Drain water
   digitalWrite(pumpPin, HIGH);
-  delay(drainTime);
+  delay(oneMin/2);
   digitalWrite(pumpPin, LOW);
-  
-  rinse(1);
 
-  rinse(2);
+  digitalWrite(solenoidPin, HIGH);
+  delay(oneMin);
+  digitalWrite(solenoidPin, LOW);
 
-  exit(0);
+  digitalWrite(motorPin, HIGH);
+  delay(oneMin*20);
+  digitalWrite(motorPin, LOW);
+
+  delay(oneMin);
+
+  digitalWrite(pumpPin, HIGH);
+  delay(oneMin/2);
+  digitalWrite(pumpPin, LOW);
+
+  //Rinse 1
+  digitalWrite(solenoidPin, HIGH);
+  delay(oneMin);
+  digitalWrite(solenoidPin, LOW);
+
+  digitalWrite(motorPin, HIGH);
+  delay(oneMin*5);
+  digitalWrite(motorPin, LOW);
+
+  // Drain water
+  digitalWrite(pumpPin, HIGH);
+  delay(oneMin/2);
+  digitalWrite(pumpPin, LOW);
+
+  // rest
+  delay(oneMin);
+
+  //Rinse 2
+  digitalWrite(solenoidPin, HIGH);
+  delay(oneMin/2);
+  digitalWrite(solenoidPin, LOW);
+
+  digitalWrite(motorPin, HIGH);
+  delay(oneMin*3);
+  digitalWrite(motorPin, LOW);
+
+  // Drain water
+  digitalWrite(pumpPin, HIGH);
+  delay(oneMin);
+  digitalWrite(pumpPin, LOW);
+
+  // rest
+  delay(oneMin);
+
+  //NOTE 67.5 minutes all up
+}
+ 
+void loop() {
+
 }
 
