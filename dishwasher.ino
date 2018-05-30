5ca0588e2a4e71baa6efc52326c7d28384f373bf
serine <tskiril@gmail.com>
2018-05-30 21:08:12 +1000

    yet another re-write

--- dishwasher.ino
+++ dishwasher.ino
@@ -1,94 +1,98 @@
 
-int solenoidPin = 8;
-int pumpPin = 9;
-int motorPin = 10;
+/*
+
+Name: Smart Dishwasher
+Author: kizza
+Date: 29.05.2018
+
+ */
+
+const int solenoidPin = 8;
+const int pumpPin = 9;
+const int motorPin = 10;
 //int soapPin = 11;
 
-unsigned long oneMin = 60000;
+const unsigned long ONE_SEC = 1000UL;
+const unsigned long TWENTY_SEC = 20UL * ONE_SEC;
+const unsigned long THIRTY_SEC = 35UL * ONE_SEC;
+const unsigned long ONE_MIN = 60UL * ONE_SEC;
+const unsigned long TWO_MIN = 120UL * ONE_SEC;
+const unsigned long THREE_MIN = 180UL * ONE_SEC;
+const unsigned long ONE_N_HALF_MIN = 100UL * ONE_SEC; // I know that this is not minute and half!
+const unsigned long FIVE_MIN = 5UL * ONE_MIN;
+const unsigned long THIRTEEN_MIN = 13UL * ONE_MIN;
+const unsigned long THIRTY_MIN = 30UL * ONE_MIN;
+const unsigned long ONE_HOUR = 60UL * ONE_MIN;
 
-void setup(){
+void driveRelay(const int relayPin, const unsigned long cycleTime) {
+
+  digitalWrite(relayPin, HIGH);
+  delay(cycleTime);
+  digitalWrite(relayPin, LOW);
+
+}
+
+void setup() {
+
   Serial.begin(9600);
   pinMode(solenoidPin, OUTPUT);
   pinMode(pumpPin, OUTPUT);
   pinMode(motorPin, OUTPUT);
+
 }
 
 void loop() {
   /* Init */
 
-  delay(oneMin/3);
-  digitalWrite(pumpPin, HIGH);
-  delay(oneMin/3);
-  digitalWrite(pumpPin, LOW);
+  delay(THIRTY_SEC);
+  driveRelay(pumpPin, THIRTY_SEC);
 
   /* Main */
 
-  //Fill up with water 
-  digitalWrite(solenoidPin, HIGH);
-  delay(oneMin*1.5);
-  digitalWrite(solenoidPin, LOW);
+  // Cycle 1
 
-  // Start wash
-  digitalWrite(motorPin, HIGH);
-  delay(oneMin*20);
-  digitalWrite(motorPin, LOW);
-
+  // fill up
+  driveRelay(solenoidPin, ONE_N_HALF_MIN);
+  // wash
+  driveRelay(motorPin, THIRTY_MIN);
   // rest
-  delay(oneMin);
+  delay(ONE_MIN);
+  // drain
+  driveRelay(pumpPin, THIRTY_SEC);
 
-  // Drain water
-  digitalWrite(pumpPin, HIGH);
-  delay(oneMin/2);
-  digitalWrite(pumpPin, LOW);
+  // Cycle 2
 
-  digitalWrite(solenoidPin, HIGH);
-  delay(oneMin*1.5);
-  digitalWrite(solenoidPin, LOW);
-
-  digitalWrite(motorPin, HIGH);
-  delay(oneMin*20);
-  digitalWrite(motorPin, LOW);
-
-  delay(oneMin*13);
-
-  digitalWrite(pumpPin, HIGH);
-  delay(oneMin/2);
-  digitalWrite(pumpPin, LOW);
+  // fill up
+  driveRelay(solenoidPin, ONE_N_HALF_MIN);
+  // wash
+  driveRelay(motorPin, THIRTY_MIN);
+  // rest
+  delay(THIRTEEN_MIN);
+  // drain
+  driveRelay(pumpPin, THIRTY_SEC);
 
   /* Rinse */
 
   // Rinse 1
-  digitalWrite(solenoidPin, HIGH);
-  delay(oneMin*1.5);
-  digitalWrite(solenoidPin, LOW);
 
-  digitalWrite(motorPin, HIGH);
-  delay(oneMin*5);
-  digitalWrite(motorPin, LOW);
-
-  // Drain water
-  digitalWrite(pumpPin, HIGH);
-  delay(oneMin/2);
-  digitalWrite(pumpPin, LOW);
-
+  // fill up
+  driveRelay(solenoidPin, ONE_N_HALF_MIN);
+  // rinse
+  driveRelay(motorPin, FIVE_MIN);
+  // drain
+  driveRelay(pumpPin, ONE_MIN);
   // rest
-  delay(oneMin*2);
+  delay(TWO_MIN);
 
-  //Rinse 2
-  digitalWrite(solenoidPin, HIGH);
-  delay(oneMin);
-  digitalWrite(solenoidPin, LOW);
+  // Rinse 2
 
-  digitalWrite(motorPin, HIGH);
-  delay(oneMin*3);
-  digitalWrite(motorPin, LOW);
+  // fill up
+  driveRelay(solenoidPin, ONE_N_HALF_MIN);
+  // rinse
+  driveRelay(motorPin, THREE_MIN);
+  // drain
+  driveRelay(pumpPin, ONE_N_HALF_MIN);
 
-  // Drain water
-  digitalWrite(pumpPin, HIGH);
-  delay(oneMin*1.5);
-  digitalWrite(pumpPin, LOW);
-
-  // Stop washing cycle
   while(1);
 }
 
